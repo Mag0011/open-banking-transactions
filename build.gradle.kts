1269598f931023ca76f76df2a10528d6379fb3c4
Enrique <enrique,magdalenoc@gmail.com>
2025-01-06 19:07:14 -0600

    feature: Added Open Api Specification for the Account and Transaction API.
     * Added yaml specification for Account and Transaction API. Thius file is nused along with
       Open Api Generator for generating source code that is added into project's sources (model).
     * Added plugin in gradle file, and below changes:
            * Added open api generator, swagger and springdoc versions to make the generated source code be compiled
            * Added necessary dependencies for genrated sources.
            * Updated spring boot version.
            * Added gradle tasks to generate API sources (openApiGenerate) and to copy generated model classes
              to current's model directory trhough a directory that is called /external. Created tasks were
              were appenmdedn to build main task.
    
     * Added git ignore to exclude external directory of being generated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+extra["swagger-ui.version"] = "5.3.1"
+extra["springdoc.version"] = "1.6.14"
+
 plugins {
     java
-    id("org.springframework.boot") version "2.7.5"
+    id("org.springframework.boot") version "2.7.15"
     id("io.spring.dependency-management") version "1.1.5"
+    id("org.openapi.generator") version "7.4.0"
 }
 val testcontainersVersion by extra("1.19.8")
 
@@ -25,19 +29,26 @@ repositories {
 }
 
 dependencies {
-    testImplementation("io.rest-assured:rest-assured:4.2.0")
-    testImplementation("org.hamcrest:hamcrest:2.2")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("mysql:mysql-connector-java:8.0.28")
-    implementation ("javax.validation:validation-api:2.0.1.Final")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+    implementation("org.springframework.data:spring-data-commons")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    testImplementation("io.rest-assured:rest-assured:4.2.0")
+    testImplementation("org.hamcrest:hamcrest:2.2")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 dependencyManagement {
     imports {
@@ -45,6 +56,36 @@ dependencyManagement {
     }
 }
 
+tasks.named("openApiGenerate", org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
+    generatorName = "spring"
+    inputSpec = "${layout.projectDirectory}/src/main/resources/open-api/account-info-openapi.yaml"
+    outputDir = "${layout.buildDirectory.get()}/generated/open-banking"
+    invokerPackage = "com.open-banking.invoker"
+    modelPackage = "com.open-banking.model"
+    apiPackage = "com.open-banking.api"
+    additionalProperties.put("interfaceOnly", "true")
+    additionalProperties.put("basePackage", "com.open-banking")
+}
+
+// TASKS
+
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.register("copyGeneratedModelIntoProject", Copy::class) {
+    copy {
+        from("${layout.buildDirectory.get()}/generated/open-banking/src/main/java/com/open_banking/model")
+        into("${layout.projectDirectory}/src/main/java/org/openbanking/com/model/external")
+        include("**/*")
+        dependsOn("openApiGenerate")
+    }
+}
+
+//tasks.bootRun {
+//    dependsOn( "openApiGenerate")
+//}
+
+tasks.build {
+    dependsOn( "copyGeneratedModelIntoProject")
 }
